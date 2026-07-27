1c974f13eb711ce17ff8c87d9a643fdd27be4229
Thunder118 <ahyari.z17@gmail.com>
2026-07-27 19:27:10 +0700

    feat(discovery-server): Set up Eureka Server for Service Discovery

---
+++ discovery-server/src/test/java/food_delivery/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package food_delivery.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
