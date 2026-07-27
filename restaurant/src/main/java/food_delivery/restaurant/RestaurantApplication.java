17b3aefd67b4728615184bc06e05c6eeac14fab1
Thunder118 <ahyari.z17@gmail.com>
2026-07-28 00:48:03 +0700

    Integrate Zipkin tracing and Prometheus metrics

--- restaurant/src/main/java/food_delivery/restaurant/RestaurantApplication.java
+++ restaurant/src/main/java/food_delivery/restaurant/RestaurantApplication.java
@@ -3,6 +3,9 @@ package food_delivery.restaurant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import zipkin2.reporter.Sender;
+import zipkin2.reporter.urlconnection.URLConnectionSender;
 
 @SpringBootApplication
 @EnableCaching
@@ -10,6 +13,11 @@ public class RestaurantApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantApplication.class, args);
+	}
+
+	@Bean
+	public Sender zipkinSender() {
+		return URLConnectionSender.create("http://localhost:9411/api/v2/spans");
 	}
 
 }
--- restaurant/src/main/java/food_delivery/restaurant/controller/TestController.java
+++ restaurant/src/main/java/food_delivery/restaurant/controller/TestController.java
@@ -1,4 +1,18 @@
 package food_delivery.restaurant.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
 public class TestController {
+
+    @GetMapping("/test-trace")
+    public String testTrace() {
+
+        log.info("Memeriksa apakah Trace ID sudah muncul di sini...");
+        return "Trace Test OK";
+    }
+
 }
