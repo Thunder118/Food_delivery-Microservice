bee798e096930d8c56c959b6dacfb5fe542bbb26
Thunder118 <ahyari.z17@gmail.com>
2026-07-28 00:47:36 +0700

    Implement Trace ID propagation across Order Service

--- order/src/main/java/food_delivery/order/service/OrderKafkaProducer.java
+++ order/src/main/java/food_delivery/order/service/OrderKafkaProducer.java
@@ -4,7 +4,9 @@ import food_delivery.order.config.KafkaTopicConfig;
 import food_delivery.order.dto.OrderCreatedEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -18,4 +20,6 @@ public class OrderKafkaProducer {
         log.info("🚀 [Kafka Producer] Mengirim event untuk Driver ID: {}", event.getDriverId());
         kafkaTemplate.send(KafkaTopicConfig.ORDER_CREATED_TOPIC, event);
     }
+
+
 }
