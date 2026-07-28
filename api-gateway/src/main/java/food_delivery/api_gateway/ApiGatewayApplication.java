61d22fef933ca54e73fced8aa245cafb1f80b47d
Thunder118 <ahyari.z17@gmail.com>
2026-07-28 22:39:59 +0700

    Set up Spring Cloud Gateway on port 8080

--- api-gateway/src/main/java/food_delivery/api_gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/food_delivery/api_gateway/ApiGatewayApplication.java
@@ -3,6 +3,12 @@ package food_delivery.api_gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import zipkin2.Span;
+import zipkin2.reporter.AsyncReporter;
+import zipkin2.reporter.Sender;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +16,15 @@ public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+
+	}
+
+	@Bean
+	public AsyncReporter<zipkin2.Span> zipkinAsyncReporter(Sender sender) {
+		return AsyncReporter.builder(sender)
+				.queuedMaxSpans(1000)
+				.messageTimeout(10, java.util.concurrent.TimeUnit.MILLISECONDS)
+				.build();
 	}
 
 }
