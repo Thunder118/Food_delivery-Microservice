fab64709d0a8cf2049ffbea8230b72ff2c8cd9bf
Thunder118 <ahyari.z17@gmail.com>
2026-07-28 23:01:30 +0700

    .

--- restaurant/src/main/java/food_delivery/restaurant/RestaurantApplication.java
+++ restaurant/src/main/java/food_delivery/restaurant/RestaurantApplication.java
@@ -3,12 +3,19 @@ package food_delivery.restaurant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import zipkin2.Span;
+import zipkin2.reporter.AsyncReporter;
 import zipkin2.reporter.Sender;
+import zipkin2.reporter.brave.ZipkinSpanHandler;
 import zipkin2.reporter.urlconnection.URLConnectionSender;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @EnableCaching
+@EnableDiscoveryClient
 public class RestaurantApplication {
 
 	public static void main(String[] args) {
@@ -17,7 +24,15 @@ public class RestaurantApplication {
 
 	@Bean
 	public Sender zipkinSender() {
-		return URLConnectionSender.create("http://localhost:9411/api/v2/spans");
+		return  URLConnectionSender.create("http:localhost:9411/api/v2/spans");
+	}
+
+	@Bean
+	public AsyncReporter<zipkin2.Span> zipkinAsyncReporter(Sender sender) {
+		return AsyncReporter.builder(sender)
+				.queuedMaxSpans(1000)
+				.messageTimeout(10, java.util.concurrent.TimeUnit.MILLISECONDS)
+				.build();
 	}
 
 }
