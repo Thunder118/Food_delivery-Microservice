7bb082311aee97237bf2fbaa29a9febfd382029f
Thunder118 <ahyari.z17@gmail.com>
2026-07-28 23:01:10 +0700

    .

--- driver/src/main/java/food_delivery/driver/DriverApplication.java
+++ driver/src/main/java/food_delivery/driver/DriverApplication.java
@@ -3,6 +3,12 @@ package food_delivery.driver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import zipkin2.Span;
+import zipkin2.reporter.AsyncReporter;
+import zipkin2.reporter.Sender;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 @EnableCaching
@@ -11,5 +17,16 @@ public class DriverApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DriverApplication.class, args);
 	}
+
+	@Bean
+	public AsyncReporter<Span> zipkinAsyncReporter(Sender sender) {
+		return AsyncReporter.builder(sender)
+				.queuedMaxSpans(100)
+				.messageTimeout(10, TimeUnit.MICROSECONDS)
+				.build();
+
+	}
+
+
 
 }
